4c1f9b02fbad0e6162fc841e3a4ef690f34ff49b
Gustavo Parra <gustavoparrabotero@gmail.com>
2021-01-17 21:47:30 -0400

    Environment config finished

--- app/src/components/Cards.js
+++ app/src/components/Cards.js
@@ -10,7 +10,7 @@ const Cards = () => {
     const [people, setPeople] = useState([]);
 
     const fetchCards = async () => {
-        const res = await axios.get('http://localhost:5000/tinder/cards');
+        const res = await axios.get('/tinder/cards');
         setPeople(res.data.cards);
     };
 
@@ -18,25 +18,13 @@ const Cards = () => {
         fetchCards();
     }, []);
 
-    const swiped = (nameToDelete) => {
-        console.log('removing ' + nameToDelete);
-    };
-
-    const outOfFrame = (name) => {
-        console.log(name + 'left the screen');
-    };
-
     return (
         <div className='cards'>
             <div className='cards-container'>
                 {
                     people.map(person => (
                         <TinderCard
-                            className='swipe'
-                            key={person.name}
-                            preventSwipe={['up', 'down']}
-                            onSwipe={dir => swiped(dir, person.name)}
-                            onCardLeftScreen={() => outOfFrame(person.name)}>
+                            className='swipe'>
                             <div style={{ backgroundImage: `url(${person.image})` }} className='card'>
                                 <h3>{person.name}</h3>
                             </div>
